6c6ae8f3c230dd346d92b1b23ca217df0a4d870e
miridium-electrode <checkerdance@yahoo.com>
2022-04-22 08:25:39 +0700

    add all

---
+++ app/Models/Pengmas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengmas extends Model
+{
+    use HasFactory;
+}
