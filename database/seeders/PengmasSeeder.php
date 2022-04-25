2ab3d63318db3e76e5e7656d39d5c4fa14f789f9
miridium-electrode <checkerdance@yahoo.com>
2022-04-25 17:21:43 +0700

    update reflect seeder

--- database/seeders/PengmasSeeder.php
+++ database/seeders/PengmasSeeder.php
@@ -17,13 +17,13 @@ class PengmasSeeder extends Seeder
         DB::table('pengmas')->insert([
             [
                 'dana' => 15000.00,
-                'id_prodi' => 1,
+                'prodi_id' => 1,
                 'judul' => "Pengenalan Penggunaan Bluetooth sebagai Media Pembelajaran Jarak Jauh Studi Kasus Belajar Pemrograman Python bagi Siswa SMK di USBJJ Pungging Mojokerto",
                 'periode' => '2019',
             ],
             [
                 'dana' => 15000.00,
-                'id_prodi' => 3,
+                'prodi_id' => 3,
                 'judul' => "Service Learning in Computer Engineering (Slice) Desa Sedayulawas, Kecamatan Brondong Kabupaten Lamongan",
                 'periode' => '2019',
             ]
