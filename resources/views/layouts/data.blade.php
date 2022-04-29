69bcbc3722f761e95d543f7898442f4c2bf5982b
miridium-electrode <checkerdance@yahoo.com>
2022-04-30 06:15:58 +0700

    swap bootstrap to come before sb-admin

--- resources/views/layouts/data.blade.php
+++ resources/views/layouts/data.blade.php
@@ -18,9 +18,9 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="css/stylesheet.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
 
 </head>
 
