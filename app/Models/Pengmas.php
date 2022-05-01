a03b51f437b484ec42f64967b278c210476697a5
miridium-electrode <checkerdance@yahoo.com>
2022-05-02 05:23:31 +0700

    pengmas model

--- app/Models/Pengmas.php
+++ app/Models/Pengmas.php
@@ -3,17 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class Pengmas extends Model
 {
     public $timestamps = false;
-
-    public function getProdi() {
-        return DB::table('prodi')->get();
+    
+    public function jurusan()
+    {
+        return $this->hasMany(Jurusan::class);
     }
 
-    public function getPengmasTest() {
-        return DB::table('pengmas')->get();
+    public function periode()
+    {
+        return $this->hasMany(Periode::class);
+    }
+
+    public function prodi() {
+        return $this->hasMany(Prodi::class);
+    }
+
+    public function pegawai()
+    {
+        return $this->belongsToMany(Pegawai::class, 
+        'daftar_pengmas', 'pengmas_id', 'nip')->withPivot('ketua');
     }
 }
