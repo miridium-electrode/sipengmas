23a36824e0e34f9e71616a0c16110aaf20e84299
miridium-electrode <checkerdance@yahoo.com>
2022-05-02 16:58:37 +0700

    add relation to pengmas model

--- app/Models/Pengmas.php
+++ app/Models/Pengmas.php
@@ -6,25 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengmas extends Model
 {
+    protected $table = 'pengmas';
     public $timestamps = false;
     
     public function jurusan()
     {
-        return $this->hasMany(Jurusan::class);
+        return $this->belongsTo(Jurusan::class);
     }
 
     public function periode()
     {
-        return $this->hasMany(Periode::class);
+        return $this->belongsTo(Periode::class);
     }
 
     public function prodi() {
-        return $this->hasMany(Prodi::class);
+        return $this->belongsTo(Prodi::class);
     }
 
-    public function pegawai()
-    {
+    public function ketua() {
         return $this->belongsToMany(Pegawai::class, 
-        'daftar_pengmas', 'pengmas_id', 'nip')->withPivot('ketua');
+        'daftar_pengmas', 'pengmas_id', 'nip')
+        ->wherePivot('ketua', 1);
+    }
+
+    public function anggota() {
+        return $this->belongsToMany(Pegawai::class, 
+        'daftar_pengmas', 'pengmas_id', 'nip')
+        ->wherePivot('ketua', 0);
     }
 }
